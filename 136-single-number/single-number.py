e1841acf81c1588b281657552a467c4a557f27fb
minhjoh <144196668+minhjoh@users.noreply.github.com>
2023-12-04 21:12:07 +0700

    Time: 93 ms (73.66%) | Memory: 15.4 MB (59.81%) - LeetSync

--- 136-single-number/single-number.py
+++ 136-single-number/single-number.py
@@ -4,11 +4,7 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        nums.sort()
-        if len(nums) == 1:
-            return nums[0]
-        else:
-            uniqNum = 0
-            for num in nums:
-                uniqNum ^= num
+        uniqNum = 0
+        for num in nums:
+            uniqNum ^= num
         return uniqNum; 
