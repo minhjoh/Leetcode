ed33a24d3e1411ff78ba35399ca289fc462773c2
minhjoh <144196668+minhjoh@users.noreply.github.com>
2024-01-04 00:30:04 +0700

    Time: 202 ms (32.95%) | Memory: 17.2 MB (19.32%) - LeetSync

---
+++ 2244-number-of-laser-beams-in-a-bank/number-of-laser-beams-in-a-bank.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def numberOfBeams(self, bank):
+        """
+        :type bank: List[str]
+        :rtype: int
+        """
+        count = count_one = temp = 0
+        for string in bank:
+            temp = count_one
+            for chr in string:
+                if chr == '1':
+                    count_one += 1
+            if temp != count_one and temp != 0:
+                count = count + temp * (count_one - temp)
+                count_one = count_one - temp
+        return count
