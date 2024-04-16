20c6d7ba64017b17eb792455fce9707efe8554e1
minhjoh <144196668+minhjoh@users.noreply.github.com>
2024-04-16 20:00:39 +0700

    Time: 150 ms (33.54%) | Memory: 12.2 MB (78.88%) - LeetSync

--- 654-maximum-binary-tree/maximum-binary-tree.py
+++ 654-maximum-binary-tree/maximum-binary-tree.py
@@ -12,9 +12,14 @@ class Solution(object):
         """
         if not nums:
             return 
-        max_num = max(nums)
+        
+        max_num = -1
+        for i, num in enumerate(nums):
+            if num > max_num:
+                max_num = num
+                max_index = i
+
         root = TreeNode(max_num)
-        max_index = nums.index(max_num)
         root.left = self.constructMaximumBinaryTree(nums[:max_index])
         root.right = self.constructMaximumBinaryTree(nums[max_index+1:])
         return root
