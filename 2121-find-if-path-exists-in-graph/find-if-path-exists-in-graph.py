761dbf0e4369127bbd3b9f2162e3caa99789a92b
minhjoh <144196668+minhjoh@users.noreply.github.com>
2024-04-29 13:53:43 +0700

    Time: 1364 ms (93.54%) | Memory: 107.4 MB (82.14%) - LeetSync

--- 2121-find-if-path-exists-in-graph/find-if-path-exists-in-graph.py
+++ 2121-find-if-path-exists-in-graph/find-if-path-exists-in-graph.py
@@ -1,33 +1,26 @@
 class UnionFind:
     def __init__(self, n):
         self.parent = list(range(n))
-        self.rank = [0] * n
-    
-    def find(self, x):
-        if self.parent[x] != x:
-            self.parent[x] = self.find(self.parent[x])
-        return self.parent[x]
-    
-    def union(self, x, y):
-        root_x = self.find(x)
-        root_y = self.find(y)
-        if root_x == root_y:
-            return
-        if self.rank[root_x] < self.rank[root_y]:
-            self.parent[root_x] = root_y
-        elif self.rank[root_x] > self.rank[root_y]:
-            self.parent[root_y] = root_x
-        else:
-            self.parent[root_y] = root_x
-            self.rank[root_x] += 1
+
+    def find(self, i):
+        if self.parent[i] != i:
+            self.parent[i] = self.find(self.parent[i])
+        return self.parent[i]
+
+    def union(self, i, j):
+        irep = self.find(i)
+        jrep = self.find(j)
+        if irep != jrep:
+            self.parent[jrep] = irep
 
 class Solution:
     def validPath(self, n: int, edges: List[List[int]], source: int, destination: int) -> bool:
-        uf = UnionFind(n)
+        n = len(range(n))
+        unionfind = UnionFind(n)
+        for i, j in edges:
+            unionfind.union(i, j)
+        return unionfind.find(source) == unionfind.find(destination)
+    
+
+    
         
-        # Union operation based on edges
-        for u, v in edges:
-            uf.union(u, v)
-        
-        # Check if source and destination are connected
-        return uf.find(source) == uf.find(destination)
