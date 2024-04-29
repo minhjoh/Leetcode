8535586fe5b2a19d036fd78905bd5b5016ca9408
minhjoh <144196668+minhjoh@users.noreply.github.com>
2024-04-29 20:52:18 +0700

    Time: 60 ms (31.26%) | Memory: 17.2 MB (29.83%) - LeetSync

--- 684-redundant-connection/redundant-connection.py
+++ 684-redundant-connection/redundant-connection.py
@@ -8,26 +8,24 @@ class UnionFind:
             self.parent[i] = self.find(self.parent[i])
         return self.parent[i]
     
-    def UnionBySize(self, irep, jrep):
+    def union_by_size(self, irep, jrep):
         isize = self.size[irep]
         jsize = self.size[jrep]
 
         if isize < jsize:
             self.parent[irep] = jrep
-            self.size[jrep] += self.size[irep]
+            self.size[jrep] += isize
         else:
             self.parent[jrep] = irep
-            self.size[irep] += self.size[jrep]
+            self.size[irep] += jsize
 
 class Solution:
     def findRedundantConnection(self, edges: List[List[int]]) -> List[int]:
-        n = len(edges)
-        uf = UnionFind(n)
-        for (i, j) in edges:
-            irep = uf.find(i-1)
-            jrep = uf.find(j-1)
+        uf = UnionFind(len(edges))
+        for i, j in edges:
+            irep = uf.find(i - 1)
+            jrep = uf.find(j - 1)
             if irep != jrep:
-                uf.UnionBySize(irep, jrep)
+                uf.union_by_size(irep, jrep)
             else:
                 return [i, j]
-            
