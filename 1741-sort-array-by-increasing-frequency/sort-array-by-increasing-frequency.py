08bec4c14f90d8e18ea907bda0178a3693c7910b
minhjoh <144196668+minhjoh@users.noreply.github.com>
2024-07-23 14:08:29 +0700

    Time: 56 ms (19.25%) | Memory: 16.6 MB (12.04%) - LeetSync

--- 1741-sort-array-by-increasing-frequency/sort-array-by-increasing-frequency.py
+++ 1741-sort-array-by-increasing-frequency/sort-array-by-increasing-frequency.py
@@ -4,25 +4,26 @@ class Solution:
         for num in nums:
             freqs[num] = 1 + freqs.get(num, 0)
 
-        heap = []
+        sort_by_num = []
         for num, freq in freqs.items():
-            heap.append([num, freq])
+            sort_by_num.append([num, freq])
         
-        heap.sort(reverse=True)
+        sort_by_num.sort(reverse=True)
 
-        res = []
-        for num, freq in heap:
-            res.append([num]*freq)
+        sort_by_freq = []
+        for num, freq in sort_by_num:
+            sort_by_freq.append([num]*freq)
         
         def len_dict(e):
             return len(e)
         
-        res.sort(key=len_dict)
+        sort_by_freq.sort(key=len_dict)
 
-        result = []
-        for li in res:
-            result += li
-        return result
+        res = []
+        for li in sort_by_freq:
+            res += li
+
+        return res
         
 
             
