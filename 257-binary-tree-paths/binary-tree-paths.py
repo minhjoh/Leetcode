02c1e1e2a233ac8bbf0f63d35fc75ff56c472014
minhjoh <144196668+minhjoh@users.noreply.github.com>
2024-07-26 22:36:10 +0700

    Time: 40 ms (31.52%) | Memory: 16.6 MB (8.08%) - LeetSync

--- 257-binary-tree-paths/binary-tree-paths.py
+++ 257-binary-tree-paths/binary-tree-paths.py
@@ -6,21 +6,16 @@
 #         self.right = right
 class Solution:
     def binaryTreePaths(self, root: Optional[TreeNode]) -> List[str]:
-        ans = []
+        def backtrack(root, string):
+            if root:
+                string += str(root.val)
+                if not root.left and not root.right:
+                    res.append(string)
+                else:
+                    string += '->'
+                    backtrack(root.left, string)
+                    backtrack(root.right, string)
 
-        def dfs(node, path):
-            if not node:
-                return
-            if not node.left and not node.right:
-                path.append(str(node.val))
-                ans.append("->".join(path))
-                path.pop()
-                return
-            path.append(str(node.val))
-            dfs(node.left, path)
-            dfs(node.right, path)
-            path.pop()
-        
-        dfs(root, [])
-        
-        return ans
+        res = []
+        backtrack(root, "")
+        return res
